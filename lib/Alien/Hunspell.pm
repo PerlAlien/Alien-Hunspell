d5687302d6ce5fd400c704b202b198597bad33f5
Graham Ollis <plicease@cpan.org>
2017-03-28 14:29:17 -0400

    better synopsis

--- lib/Alien/Hunspell.pm
+++ lib/Alien/Hunspell.pm
@@ -6,42 +6,7 @@ use parent 'Alien::Base';
 
 # ABSTRACT: Install hunspell
 # VERSION
-
-=head1 SYNOPSIS
-
-Build.PL:
-
- use Alien::Hunspell;
- use Module::Build;
- 
- Module::Build->new(
-   ...
-   extra_compiler_flags => Alien::Hunspell->cflags,
-   extra_linker_flags   => Alien::HunSpell->libs,
-   ...
- )->create_build_script;
-
-Makefile.PL:
-
- use Alien:Hunspell;
- use ExtUtils::MakeMaker;
- 
- WriteMakefile(
-   ...
-   CCFLAGS => $alien->cflags,
-   LIBS    => $alien->libs,
-   ...
- );
-
-FFI::Platypus:
-
- use Alien::Hunspell;
- use FFI::Platypus;
- 
- my $ffi = FFI::Platypus->new(
-   lib => [Alien::Hunspell->new->dynamic_libs],
- );
- ...
+# ALIEN SYNOPSIS
 
 =head1 DESCRIPTION
 
